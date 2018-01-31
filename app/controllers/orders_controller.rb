e430a2793ea1347585b7b4b00827a6c71d55ca22
Carlos e Thiago Hocsis <pair03@campuscode.com.br>
2018-01-31 17:47:50 -0200

    [wip] costumer and order association

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  # <%= link_to "Add Product", new_product_path(:param1 => "value1", :param2 => "value2") %>
 
   def index
     if current_seller.admin?
@@ -12,13 +13,19 @@ class OrdersController < ApplicationController
     @order = Order.new
     #json = '[{"name": "Hospedagem", "id": 1, "periodicity": [{"period: "1 mês", value: "100,00"}]}]'
 
+    #@order.customer_id = params[:customer_id]
+    set_session('customer_id', params[:customer_id])
+
+    if category
+      render new_produto
+
     @categories = get_categories
   end
 
   def create
     @order = Order.new order_params
 
-    @order.customer_id = current_seller.id
+    @order.customer_id = params[:customer_id]
     @order.seller_id = current_seller.id
 
     if @order.save
@@ -37,6 +44,11 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_session(session_name, session_value)
+    session_name = session_name.to_hash
+    session[session_name] = session_value
+  end
+
   def order_params
     params.require(:order).permit(:category_id)
   end
@@ -52,6 +64,9 @@ class OrdersController < ApplicationController
     end
 
     categories
+  end
+
+  def get_products
   end
 
 end
