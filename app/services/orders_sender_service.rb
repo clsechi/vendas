78f0e994e9db634bae49d7b571832aefe055c387
IngridRodrigues15 <ingrid.s.rodrigues@hotmail.com>
2018-02-02 18:13:17 -0200

    Correcoes

--- app/services/orders_sender_service.rb
+++ app/services/orders_sender_service.rb
@@ -9,13 +9,8 @@ module OrdersSenderService
 
     def self.send_post(order)
       body = order.to_json
-      options = { :body => body }
-
+      options = { "body": body }
       self.post('/orders/new', options)
-
-      # uri = URI('https://06162072-025d-475c-883f-eb3a7407ffe6/orders/new)
-      #  res = Net::HTTP.post_form(uri, 'body' => body)
-      #  puts res.body
     end
   end
 end
