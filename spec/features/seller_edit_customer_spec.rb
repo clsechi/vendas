0ac03977bbb5bf4181dd751b3efcf8e522fc74c1
Talarini e Ingrid <pair01@campuscode.com.br>
2018-02-06 14:51:27 -0200

    add Edit customer feature

---
+++ spec/features/seller_edit_customer_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+feature 'seller edit customer' do
+  scenario 'succesfully' do
+    seller = create(:seller)
+    customer = create(:customer, :legal)
+
+    login_as(seller)
+    visit root_path
+
+    fill_in 'Busca', with: customer.name
+    click_on 'Pesquisar cliente'
+
+    click_on customer.name
+    click_on 'Editar'
+
+    fill_in 'Nome', with: 'Maria'
+    fill_in 'Endereco', with: 'rua das flores'
+    fill_in 'CPF', with: '777777777-77'
+    fill_in 'Email', with: 'email@email.com'
+    fill_in 'Telefone', with: '1199999999'
+    fill_in 'Data de Nascimento', with: '1988-02-29'
+
+    click_on 'Enviar'
+
+    expect(page).to have_content('Maria')
+    expect(page).to have_content('rua das flores')
+    expect(page).to have_content('777777777-77')
+    expect(page).to have_content('email@email.com')
+    expect(page).to have_content('1988-02-29')
+  end
+  scenario 'and must fill all fields' do
+    seller = create(:seller)
+    customer = create(:customer, :legal)
+
+    login_as(seller)
+    visit root_path
+
+    fill_in 'Busca', with: customer.name
+    click_on 'Pesquisar cliente'
+
+    click_on customer.name
+    click_on 'Editar'
+
+    fill_in 'Nome', with: ''
+    fill_in 'Endereco', with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Nome da Companhia', with: ''
+    fill_in 'Contato', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_content('Voce deve preencher todos os campos')
+  end
+end
