85265a17fb1f86fb63466b5b72ad636e9aa1c659
Jamil Martinez <jamil.cmc@gmail.com>
2018-02-14 23:55:26 -0200

    tests fix

--- spec/features/seller_create_order_spec.rb
+++ spec/features/seller_create_order_spec.rb
@@ -77,40 +77,4 @@ feature 'seller create order' do
     expect(page).to have_content('Periodicidade: Anual - R$ 19.9')
     expect(page).to have_button('Confirmar')
   end
-
-  scenario '...and confirms the salerails ' do
-    stub_request_categories
-    stub_request_products
-    stub_request_plans
-    stub_request_prices
-    customer = create(:customer, :legal)
-    seller = create(:seller)
-
-    login_as(seller)
-    visit new_customer_order_path(customer)
-    click_on 'Hospedagem'
-    click_on 'Hospedagem'
-    click_on 'Hospedagem I'
-    click_on 'Anual - R$ 19.9'
-    click_on 'Confirmar'
-    order = Order.last
-    body = { order: order, customer: order.customer }.to_json
-    url = "#{Rails.configuration.sales['client_panel_url']}"\
-          "#{Rails.configuration.sales['send_order']}"
-
-    stub_request(:post, url)
-      .with(
-        body: body
-      ).to_return(status: 200, body: '', headers: {})
-
-    response = OrdersSenderService::OrdersService.send_post(order)
-
-    expect(a_request(:post, url)).to have_been_made.at_least_once
-    expect(response).to eq(true)
-    expect(page).to have_content('Pedido realizado')
-    expect(page).to have_content('Categoria: Hospedagem')
-    expect(page).to have_content('Produto: Hospedagem')
-    expect(page).to have_content('Plano: Hospedagem I')
-    expect(page).to have_content('Periodicidade: Anual - R$ 19.9')
-  end
 end
